d7b37512b28938ccd6d87875b8070ff661d02055
Lucas <luckasmacedo2@gmail.com>
2024-03-20 23:18:28 -0300

    Purchase header entity

---
+++ lib/models/purchase_header.dart
@@ -0,0 +1,25 @@
+import 'package:my_financial_life/models/purchase_category.dart';
+
+class PurchaseHeader {
+  final String id;
+  double totalValue;
+  double notPaidValue;
+  final String description;
+  int installmentsQuantity;
+  DateTime startDate;
+  DateTime? nextPayDate;
+  final String categoryId;
+  final bool fullPaid;
+
+  PurchaseHeader({
+    required this.id,
+    required this.totalValue,
+    required this.notPaidValue,
+    required this.description,
+    required this.installmentsQuantity,
+    required this.startDate,
+    required this.nextPayDate,
+    required this.categoryId,
+    required this.fullPaid,
+  });
+}
--- lib/services/purchase_service.dart
+++ lib/services/purchase_service.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:my_financial_life/data/cache_manager.dart';
 import 'package:my_financial_life/exceptions/http_exception.dart';
 import 'package:my_financial_life/models/purchase.dart';
+import 'package:my_financial_life/models/purchase_header.dart';
 import 'package:my_financial_life/utils/cache_constants.dart';
 import 'package:my_financial_life/utils/constants.dart';
 import 'package:my_financial_life/utils/utils.dart';
@@ -16,6 +17,12 @@ class PurchaseService with ChangeNotifier {
   List<Purchase> get items => [..._items];
   int get itemsCount {
     return _items.length;
+  }
+
+  List<PurchaseHeader> _itemsHeader = [];
+  List<PurchaseHeader> get itemsHeader => [..._itemsHeader];
+  int get _itemsHeaderCount {
+    return _itemsHeader.length;
   }
 
   double sumValues() {
@@ -88,7 +95,106 @@ class PurchaseService with ChangeNotifier {
       );
     });
     _items.sort((a, b) => a.date.compareTo(b.date));
+    _setPurchaseHeader();
     notifyListeners();
+  }
+
+  void _setPurchaseHeader() {
+    var groupedPurchases = getAllDistinctKeys();
+
+    for (int i = 0; i < groupedPurchases.length; i++) {
+      String billId = groupedPurchases[i].billId;
+      List<Purchase> purchases = getPurchaseByBillId(billId);
+
+      bool fullPaid = purchases.length == countPurchasesPaidByBillId(purchases);
+
+      _itemsHeader.add(
+        PurchaseHeader(
+          id: groupedPurchases[i].billId,
+          totalValue: sumPurchasesByBillId(purchases),
+          notPaidValue: sumPurchasesNotPaidByBillId(purchases),
+          description: groupedPurchases[i].description,
+          installmentsQuantity: purchases.length,
+          startDate: getMinimumDate(purchases),
+          nextPayDate: geNextPaidDate(purchases),
+          categoryId: groupedPurchases[i].categoryId,
+          fullPaid: fullPaid,
+        ),
+      );
+    }
+  }
+
+  getMinimumDate(List<Purchase> purchases) {
+    if (purchases.length == 0) {
+      return null; // Retorna null se a lista estiver vazia
+    }
+
+    DateTime minDate = purchases[0].date;
+
+    purchases.forEach((purchase) {
+      if (purchase.date.isBefore(minDate)) {
+        minDate = purchase.date;
+      }
+    });
+
+    return minDate;
+  }
+
+  DateTime? geNextPaidDate(List<Purchase> purchases) {
+    // Filtra as compras onde paid é true
+    List<Purchase> paidPurchases =
+        purchases.where((purchase) => !purchase.paid).toList();
+
+    if (paidPurchases.isEmpty) {
+      return null; // Retorna null se não houver compras pagas na lista
+    }
+
+    // Encontra a data mínima entre as compras pagas
+    DateTime minPaidDate = paidPurchases[0].date;
+    for (var purchase in paidPurchases) {
+      if (purchase.date.isBefore(minPaidDate)) {
+        minPaidDate = purchase.date;
+      }
+    }
+
+    return minPaidDate;
+  }
+
+  getAllDistinctKeys() {
+    Map<String, Purchase> distinctMap = {};
+    for (var purchase in _items) {
+      distinctMap.putIfAbsent(purchase.billId, () => purchase);
+    }
+
+    return distinctMap.values.toList();
+  }
+
+  List<Purchase> getPurchaseByBillId(String billId) {
+    return _items.where((purchase) => purchase.billId == billId).toList();
+  }
+
+  double sumPurchasesByBillId(List<Purchase> purchases) {
+    double total = 0.0;
+    purchases.forEach(
+      (purc) {
+        total += purc.value;
+      },
+    );
+    return total;
+  }
+
+  double sumPurchasesNotPaidByBillId(List<Purchase> purchases) {
+    double total = 0.0;
+    purchases.forEach(
+      (purc) {
+        if (!purc.paid) total += purc.value;
+      },
+    );
+    return total;
+  }
+
+  int countPurchasesPaidByBillId(List<Purchase> purchases) {
+    return purchases.where((purchase) => purchase.paid).length;
   }
 
   Future<void> savePurchase(Map<String, Object> data) async {
@@ -112,7 +218,6 @@ class PurchaseService with ChangeNotifier {
       double valuePerMounth = purc.value / purc.installmentsQuantity;
       int orignalInstallmentsQuantity = purc.installmentsQuantity;
       DateTime originalDate = purc.date;
-      // Purchase newPurc = ...purc;
       for (int i = 0; i < orignalInstallmentsQuantity; i++) {
         print(i.toString() + '>> ' + purc.date.toString());
         purc.date = Utils().addMonthsToDate(originalDate, i);
